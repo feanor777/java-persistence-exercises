3a42900627f863de229d276c4ef538ba95e5c1c1
Max <m.sharov@spd-ukraine.com>
2019-04-10 07:34:49 +0300

    Implemented query helper and daoImpl

--- account-dao/src/main/java/ua/procamp/dao/AccountDaoImpl.java
+++ account-dao/src/main/java/ua/procamp/dao/AccountDaoImpl.java
@@ -1,45 +1,88 @@
 package ua.procamp.dao;
 
-import ua.procamp.model.Account;
-
-import javax.persistence.EntityManagerFactory;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import ua.procamp.exception.AccountDaoException;
+import ua.procamp.model.Account;
+
 public class AccountDaoImpl implements AccountDao {
-    private EntityManagerFactory emf;
+  private EntityManagerFactory emf;
 
-    public AccountDaoImpl(EntityManagerFactory emf) {
-        this.emf = emf;
-    }
+  public AccountDaoImpl(EntityManagerFactory emf) {
+    this.emf = emf;
+  }
 
-    @Override
-    public void save(Account account) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+  @Override
+  public void save(Account account) {
+    EntityManager entityManager = emf.createEntityManager();
+    entityManager.getTransaction().begin();
+    try {
+      entityManager.persist(account);
+    } catch (Exception e) {
+      throw new AccountDaoException(e.getMessage(), e);
     }
+    entityManager.getTransaction().commit();
+    entityManager.close();
+  }
 
-    @Override
-    public Account findById(Long id) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
-    }
+  @Override
+  public Account findById(Long id) {
+    EntityManager entityManager = emf.createEntityManager();
+    entityManager.getTransaction().begin();
+    Account account = entityManager.find(Account.class, id);
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return account;
+  }
 
-    @Override
-    public Account findByEmail(String email) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
-    }
+  @Override
+  public Account findByEmail(String email) {
+    EntityManager entityManager = emf.createEntityManager();
+    entityManager.getTransaction().begin();
+    Account account = entityManager.createQuery("SELECT a FROM Account a WHERE a.email = :email", Account.class)
+        .setParameter("email", email)
+        .getSingleResult();
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return account;
+  }
 
-    @Override
-    public List<Account> findAll() {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
-    }
+  @Override
+  public List<Account> findAll() {
+    EntityManager entityManager = emf.createEntityManager();
+    entityManager.getTransaction().begin();
+    List<Account> accounts = entityManager.createQuery("SELECT a FROM Account a", Account.class)
+        .getResultList();
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return accounts;
+  }
 
-    @Override
-    public void update(Account account) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+  @Override
+  public void update(Account account) {
+    EntityManager entityManager = emf.createEntityManager();
+    entityManager.getTransaction().begin();
+    try {
+      entityManager.merge(account);
+    } catch (Exception e) {
+      entityManager.getTransaction().rollback();
+      throw new AccountDaoException(e.getMessage(), e);
     }
+    entityManager.getTransaction().commit();
+    entityManager.close();
+  }
 
-    @Override
-    public void remove(Account account) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
-    }
+  @Override
+  public void remove(Account account) {
+    EntityManager entityManager = emf.createEntityManager();
+    entityManager.getTransaction().begin();
+    Account accountPersisted = entityManager.find(Account.class, account.getId());
+    entityManager.remove(accountPersisted);
+    entityManager.getTransaction().commit();
+    entityManager.close();
+  }
 }
 
